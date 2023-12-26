f046eb86afb0d788998fe754c6d8a0f01b17922b
chae <ddolchae513@gmail.com>
2023-12-26 23:21:00 +0900

    Stack 구현(C#)

---
+++ Stack/MyStack.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class MyStack<T>
+{
+    private List<T> items;
+
+    public MyStack()
+    {
+        items = new List<T>();
+    }
+
+    public void Push(T item)
+    {
+        items.Add(item);
+    }
+
+    public T Pop()
+    {
+        if (IsEmpty())
+        {
+            throw new InvalidOperationException("Stack is Empty");
+        }
+        T top = items[items.Count - 1];
+        items.RemoveAt(items.Count - 1);
+        return top;
+    }
+
+    public T Top()
+    {
+        if (IsEmpty())
+        {
+            throw new InvalidOperationException("Stack is Empty");
+        }
+        T top = items[items.Count - 1];
+        return top;
+    }
+
+    public bool IsEmpty()
+    {
+        return items.Count == 0;
+    }
+
+    public int Size()
+    {
+        return items.Count;
+    }
+}
+
